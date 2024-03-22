7f690b9801d8abe68166256ff4ca42fb25b96f6e
attrest <attrestmedia@gmail.com>
2024-03-22 17:24:36 +0900

    Revert "vue3 기본 라우터 셋팅"
    This reverts commit 91a8c04fb39edf0b72eadb45a689bb13827b3214.

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div id="app">
-    <ul>
-      <li><router-link to="/">Home</router-link></li>
-      <li><router-link to="/about">About</router-link></li>
-    </ul>
-    <router-view></router-view>
-  </div>
-</template>
